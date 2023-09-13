c0b2954f0927821447110b778db4dc4ae9787bd1
mohammad osama <okosama06@gmail.com>
2023-09-13 17:06:22 +0530

    Model All page is Done✔

---
+++ Backend/model/product.model.js
@@ -0,0 +1,18 @@
+const mongoose=require("mongoose");
+
+const productSchema=mongoose.Schema({
+    title:{type:String,required:true},
+    stock:{type:Number,default:190},
+    cost:{type:Number,required:true},
+    image:{type:String,required:true},
+    category:{type:String,default:"other"},
+    brand:{type:String,default:"other"},
+    date:{type:Date,default:Date.now()}
+
+})
+
+
+const Productmodel=mongoose.model("product",productSchema);
+
+
+module.exports={Productmodel}
