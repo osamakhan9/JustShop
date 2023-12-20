c7a70b0f27a355b734e3a556841af93379ad0d30
MOHAMMAD OSAMA <101393695+osamakhan9@users.noreply.github.com>
2023-12-20 20:58:58 +0530

    Update Nav

--- Frontend/src/Components/Navbar/NavbarMobile.jsx
+++ Frontend/src/Components/Navbar/NavbarMobile.jsx
@@ -23,7 +23,7 @@ function NavbarMobile() {
           <DrawerOverlay />
           <DrawerContent>
             <DrawerCloseButton />
-            <DrawerHeader>Finest Shave</DrawerHeader>
+            <DrawerHeader>Just Shop</DrawerHeader>
   
             <DrawerBody bgColor={"rgb(0,18,51)"}>
               <VStack color="rgb(239,224,201)">
